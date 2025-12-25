93daecccfdb8e8467a897a86e0c4e4c376bb01eb
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2025-12-25 23:09:34 +0100

    feat(header): add stat-card to header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { StatCardComponent } from "../stat-card/stat-card.component";
+import { Stat } from 'src/app/core/models/olympic.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	standalone: true,
-	styleUrl: './header.component.scss'
+	styleUrl: './header.component.scss',
+ imports: [StatCardComponent, CommonModule]
 })
 export class HeaderComponent {
-	@Input() title: string = '';
+	@Input() title!: string;
+	@Input() stats!: Stat[];
 }
