0059e8228334e1a4dbd1e12def3b4892942b78dc
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2025-12-25 18:24:13 +0100

    refacto(country): replace title by header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { CountryComponent } from "./pages/country/country.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, CountryComponent],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -1,13 +1,16 @@
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {ActivatedRoute, ParamMap, Router, RouterLink} from '@angular/router';
 import Chart from 'chart.js/auto';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
-  styleUrls: ['./country.component.scss']
+  styleUrls: ['./country.component.scss'],
+  standalone: true,
+  imports: [HeaderComponent, RouterLink]
 })
 export class CountryComponent implements OnInit {
   private olympicUrl = './assets/mock/olympic.json';
