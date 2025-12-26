f17ee717c7a04ef868a95d5f6b43edc18622e17d
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2025-12-26 10:18:18 +0100

    feat(service): add getOlympics method

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Olympic, Participation } from '../models/olympic.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OlympicService {
+  private apiUrl = './assets/mock/olympic.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getOlympics(): Observable<Olympic[]> {
+    return this.http.get<Olympic[]>(this.apiUrl).pipe(
+      catchError(error => {
+        return throwError(() => new Error(`Unable to load olympic data: ${error.error}`));
+      })
+    );
+  }
 }
